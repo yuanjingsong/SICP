95b1b03df1f1fa3d3eba0e7c2a515bc92403ed13
yuanjingsong <532473857@qq.com>
2018-08-01 23:59:23 +0800

    update

---
+++ 1-21.scm
@@ -0,0 +1,10 @@
+(define (smallest-divisor n ) (find-divisor n 2))
+(define (find-divisor n test-divisor) 
+  (cond  (( > (square test-divisor) n) n)
+         ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ 1 test-divisor)))
+  )
+)
+(define (square n) (* n n))
+(define (divides? a b) (= (remainder b a) 0))
+
