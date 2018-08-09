a854351e03286663ad5d573945561ab2b428d0f2
yuanjingsong <532473857@qq.com>
2018-08-09 09:02:59 +0800

    update

--- expmod.scm
+++
@@ -1,7 +0,0 @@
-(define (expmod base exp m) 
-  (cond ((= exp 0) 1)
-        ( (even? exp) (remainder (square(expmod base (/ exp 2) m)) m))
-        ( else (remainder (* base (expmod base (- exp 1) m)) m ))
-    )
-  )
-(define (square m) (* m m ))
